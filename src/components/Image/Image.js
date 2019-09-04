4df7035d60ce42e6dec1df72d0d023166cac9fbb
Oswaldo Andrés <48687178+Oswalman@users.noreply.github.com>
2019-09-04 14:02:07 -0500

    Primer commit
    sds

---
+++ src/components/Image/Image.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import React, { Component } from './node_modules/react';
+
+const Image=()=>{
+
+ 
+        return(
+           
+           <img src={'../img/image.jpg'}></img>
+        );
+        
+        
+
+    }
+export default Image;
---
+++ src/views/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  Image  from '../../components/Image';
+const About = props => {
+  return (
+    <div>
+      <h3>Acerca de Oswaldo Andrés Angarita León
+        Jhodman Contreras
+      </h3>
+      <Image />
+      
+    </div>
+  );
+};
+
+export default About;
